b82e9e5cae8e4b59044598f75073c00bd753cc56
Mmdshorang <mmd.sk72000@gamil.com>
2026-05-31 13:50:57 +0330

    change handle copy be khate https

--- frontend/src/components/CopyText.jsx
+++ frontend/src/components/CopyText.jsx
@@ -1,8 +1,27 @@
 const CopyText = ({ value }) => {
-  const handleCopy = () => {
-    navigator.clipboard.writeText(value || "");
-  };
+  const handleCopy = (text) => {
+    try {
+      if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(text);
+      } else {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
 
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        document.execCommand("copy");
+
+        textArea.remove();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="flex items-center justify-between gap-3">
       <span className="text-sm text-gray-700">{value || "-"}</span>
